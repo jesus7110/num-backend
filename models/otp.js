7e0ef5e2bc74000d027a7d89f9d3d44f38aa311e
jesus7110 <juthikahazra11@gmail.com>
2024-01-14 12:53:30 +0530

    Login OTP

---
+++ models/otp.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const Otp = new mongoose.Schema({
+
+    mobileNumber: {
+        type: String,
+        required: true
+    },
+    otp: {
+        type: String,
+        required: true
+    },
+    createdAt: { type: Date, default: Date.now, index: { expires: 300 } }
+
+    // After 5 minutes it deleted automatically from the database
+}, { timestamps: true })
+
+const OTP_Model = mongoose.model('OTP', Otp);
+
+module.exports = OTP_Model;
--- models/user.js
+++ models/user.js
@@ -4,11 +4,15 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     mobileNumber: { type: String, required: true, unique: true },
-    password: { type: String, required: true }, // Store hashed password
     bankDetails: {
         // Structure for bank details
+        accountName: { type: String, required: true, unique: true },
+        accountNumber: { type: String, required: true, unique: true },
+        ifscCode: { type: String, required: true, unique: true },
+        accountTYpe: { type: String, enum: ['savings','current'], required: true, unique: true },
     },
     walletBalance: { type: Number, default: 0 },
+    isKyc: { type: Boolean, default:false, required: true, unique: true },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
 });
